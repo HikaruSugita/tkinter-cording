c268ba7ffffa29a7fc8f976fe137d3e9a9daefff
HikaruSugita <213132h@SugitanoMacBook-Air.local>
2024-01-30 14:29:31 +0900

    ボール、フィールド追加

--- gui.py
+++ gui.py
@@ -1,8 +1,21 @@
 import tkinter as t
 import time
 
-window_width = 1400
-window_height = 1000
+window_width = 700 
+window_height = 500
+
+canvas_width = 950
+canvas_height = 800
+
+# co→ball: 跳ね返るボール
+ball = {
+    "X": 350, "y": 250,
+    "dx": 5, "dy": 5,
+    "r": 5 
+}
+
+def create_circre(field, x, y, r, **kwargs):
+    return field.create_oval(x-r, y-r, x+r,y+r, **kwargs)
 
 def main():
     window = t.Tk()
@@ -13,7 +26,11 @@ def main():
 
     guzai = t.Label(window, text="Hello World!")
     guzai.place(x=0, y=0)
-    
+
+    field = t.Canvas(window, width=canvas_width, height=canvas_height, bg="black")
+    #field.place(x=(window_width-canvas_width)/2, y=(window_height-canvas_height)/2)
+    field.place(x=0, y=0)
+
     window.mainloop()
 
 if __name__ == "__main__":
